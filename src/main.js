a7c4864e787170e078c37f4454f5989afb535c11
Scott Westover <scottwestover2006@gmail.com>
2023-08-28 13:15:23 -0400

    added monsters to battle scene
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Phaser from './lib/phaser.js';
 import { SCENE_KEYS } from './scenes/scene-keys.js';
 import { PreloadScene } from './scenes/preload-scene.js';
+import { BattleScene } from './scenes/battle-scene.js';
 
 const game = new Phaser.Game({
-  parent: 'game-container',
+  type: Phaser.CANVAS,
+  pixelArt: false,
+  scale: {
+    parent: 'game-container',
+    width: 1024,
+    height: 576,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  backgroundColor: '#000000',
 });
 
 game.scene.add(SCENE_KEYS.PRELOAD_SCENE, PreloadScene);
+game.scene.add(SCENE_KEYS.BATTLE_SCENE, BattleScene);
 game.scene.start(SCENE_KEYS.PRELOAD_SCENE);
---
+++ src/scenes/battle-scene.js
@@ -0,0 +1,24 @@
+import {
+  BATTLE_BACKGROUND_ASSET_KEYS,
+  MONSTER_ASSET_KEYS,
+} from '../assets/asset-keys.js';
+import Phaser from '../lib/phaser.js';
+import { SCENE_KEYS } from './scene-keys.js';
+
+export class BattleScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: SCENE_KEYS.BATTLE_SCENE,
+    });
+  }
+
+  create() {
+    console.log(`[${BattleScene.name}:create] invoked`);
+    // create main background
+    this.add.image(0, 0, BATTLE_BACKGROUND_ASSET_KEYS.FOREST).setOrigin(0);
+
+    // render out the player and enemy monsters
+    this.add.image(768, 144, MONSTER_ASSET_KEYS.CARNODUSK, 0);
+    this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
+  }
+}
