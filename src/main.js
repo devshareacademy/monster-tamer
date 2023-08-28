8c73d1bd474913614c52a36eb31fa6edc345b712
Scott Westover <scottwestover2006@gmail.com>
2023-08-28 12:09:31 -0400

    loaded in initial game assets
    created a new phaser 3 scene, PreloadScene, class that is responsible
    for loading in all of the assets that used in the game.
    loaded in the initial game assets and verified that the image is displaying
    on the html canvas element.
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Phaser from './lib/phaser.js';
+import { SCENE_KEYS } from './scenes/scene-keys.js';
+import { PreloadScene } from './scenes/preload-scene.js';
 
 const game = new Phaser.Game({
   parent: 'game-container',
 });
+
+game.scene.add(SCENE_KEYS.PRELOAD_SCENE, PreloadScene);
+game.scene.start(SCENE_KEYS.PRELOAD_SCENE);
