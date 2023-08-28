e0c1bbcd03b1973161d8067e389eca6fc3bc9e19
Scott Westover <scottwestover2006@gmail.com>
2023-08-28 08:22:12 -0400

    reset repo to initial state
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/scene-keys.js
+++
@@ -1,4 +0,0 @@
-export const SCENE_KEYS = Object.freeze({
-  PRELOAD_SCENE: 'PRELOAD_SCENE',
-  BATTLE_SCENE: 'BATTLE_SCENE',
-});
--- src/utils/logging.js
+++
@@ -1,2 +0,0 @@
-export const PHASER_SCENE_LOG_STYLE =
-  'color: orange; background: black; font-size: 16px;';
