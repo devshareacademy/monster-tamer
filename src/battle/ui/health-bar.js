f63341b76f33a667c5ec4d2f0d305f2dc24d6061
Scott Westover <scottwestover2006@gmail.com>
2023-11-03 14:20:37 -0400

    revert changes to match 1st part of the video
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/battle/ui/health-bar.js
+++ src/battle/ui/health-bar.js
@@ -16,12 +16,6 @@ export class HealthBar {
   #middle;
   /** @type {Phaser.GameObjects.Image} */
   #rightCap;
-  /** @type {Phaser.GameObjects.Image} */
-  #leftShadowCap;
-  /** @type {Phaser.GameObjects.Image} */
-  #middleShadow;
-  /** @type {Phaser.GameObjects.Image} */
-  #rightShadowCap;
 
   /**
    * @param {Phaser.Scene} scene the Phaser 3 Scene the battle menu will be added to
@@ -34,7 +28,6 @@ export class HealthBar {
     this.#scaleY = 0.7;
 
     this.#healthBarContainer = this.#scene.add.container(x, y, []);
-    this.#createHealthBarShadowImages(x, y);
     this.#createHealthBarImages(x, y);
     this.#setMeterPercentage(1);
   }
@@ -44,33 +37,8 @@ export class HealthBar {
   }
 
   /**
-   * @param {number} x the x position to place the health bar game object
-   * @param {number} y the y position to place the health bar game object
-   * @returns {void}
-   */
-  #createHealthBarShadowImages(x, y) {
-    this.#leftShadowCap = this.#scene.add
-      .image(x, y, HEALTH_BAR_ASSET_KEYS.LEFT_CAP_SHADOW)
-      .setOrigin(0, 0.5)
-      .setScale(1, this.#scaleY);
-
-    this.#middleShadow = this.#scene.add
-      .image(this.#leftShadowCap.x + this.#leftShadowCap.width, y, HEALTH_BAR_ASSET_KEYS.MIDDLE_SHADOW)
-      .setOrigin(0, 0.5)
-      .setScale(1, this.#scaleY);
-    this.#middleShadow.displayWidth = this.#fullWidth;
-
-    this.#rightShadowCap = this.#scene.add
-      .image(this.#middleShadow.x + this.#middleShadow.displayWidth, y, HEALTH_BAR_ASSET_KEYS.RIGHT_CAP_SHADOW)
-      .setOrigin(0, 0.5)
-      .setScale(1, this.#scaleY);
-
-    this.#healthBarContainer.add([this.#leftShadowCap, this.#middleShadow, this.#rightShadowCap]);
-  }
-
-  /**
-   * @param {number} x the x position to place the health bar game object
-   * @param {number} y the y position to place the health bar game object
+   * @param {number} x the x position to place the health bar container
+   * @param {number} y the y position to place the health bar container
    * @returns {void}
    */
   #createHealthBarImages(x, y) {
@@ -92,39 +60,10 @@ export class HealthBar {
     this.#healthBarContainer.add([this.#leftCap, this.#middle, this.#rightCap]);
   }
 
-  /**
-   * @param {number} [percent=1] a number between 0 and 1 that is used for setting how filled the health bar is
-   */
   #setMeterPercentage(percent = 1) {
     const width = this.#fullWidth * percent;
 
     this.#middle.displayWidth = width;
     this.#rightCap.x = this.#middle.x + this.#middle.displayWidth;
-  }
-
-  /**
-   *
-   * @param {number} [percent=1] a number between 0 and 1 that is used for setting how filled the health bar is
-   * @param {Object} [options]
-   * @param {number} [options.duration=1000]
-   * @param {() => void} [options.callback]
-   */
-  setMeterPercentageAnimated(percent, options) {
-    const width = this.#fullWidth * percent;
-
-    this.#scene.tweens.add({
-      targets: this.#middle,
-      displayWidth: width,
-      duration: options?.duration || 1000,
-      ease: Phaser.Math.Easing.Sine.Out,
-      onUpdate: () => {
-        this.#rightCap.x = this.#middle.x + this.#middle.displayWidth;
-        const isVisible = this.#middle.displayWidth > 0;
-        this.#leftCap.visible = isVisible;
-        this.#middle.visible = isVisible;
-        this.#rightCap.visible = isVisible;
-      },
-      onComplete: options?.callback,
-    });
   }
 }
--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -29,7 +29,6 @@ export class BattleScene extends Phaser.Scene {
     this.add.image(256, 316, MONSTER_ASSET_KEYS.IGUANIGNITE, 0).setFlipX(true);
 
     // render out the player health bar
-    const playerHealthBar = new HealthBar(this, 34, 34);
     const playerMonsterName = this.add.text(30, 20, MONSTER_ASSET_KEYS.IGUANIGNITE, {
       color: '#7E3D3F',
       fontSize: '32px',
@@ -37,7 +36,7 @@ export class BattleScene extends Phaser.Scene {
     this.add.container(556, 318, [
       this.add.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0),
       playerMonsterName,
-      playerHealthBar.container,
+      new HealthBar(this, 34, 34).container,
       this.add.text(playerMonsterName.width + 35, 23, 'L5', {
         color: '#ED474B',
         fontSize: '28px',
@@ -56,7 +55,6 @@ export class BattleScene extends Phaser.Scene {
     ]);
 
     // render out the enemy health bar
-    const enemyHealthBar = new HealthBar(this, 34, 34);
     const enemyMonsterName = this.add.text(30, 20, MONSTER_ASSET_KEYS.CARNODUSK, {
       color: '#7E3D3F',
       fontSize: '32px',
@@ -64,7 +62,7 @@ export class BattleScene extends Phaser.Scene {
     this.add.container(0, 0, [
       this.add.image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND).setOrigin(0).setScale(1, 0.8),
       enemyMonsterName,
-      enemyHealthBar.container,
+      new HealthBar(this, 34, 34).container,
       this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
         color: '#ED474B',
         fontSize: '28px',
@@ -81,12 +79,6 @@ export class BattleScene extends Phaser.Scene {
     this.#battleMenu.showMainBattleMenu();
 
     this.#cursorKeys = this.input.keyboard.createCursorKeys();
-    playerHealthBar.setMeterPercentageAnimated(0.5, {
-      duration: 3000,
-      callback: () => {
-        console.log('animation completed');
-      },
-    });
   }
 
   update() {
