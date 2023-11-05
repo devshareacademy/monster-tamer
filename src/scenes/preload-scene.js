d7353411992ba520eac6ed5d2955d2dff4d9a604
Scott Westover <scottwestover2006@gmail.com>
2023-11-05 08:30:38 -0500

    fixed conflict
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -40,18 +40,6 @@ export class PreloadScene extends Phaser.Scene {
       HEALTH_BAR_ASSET_KEYS.LEFT_CAP,
       `${kenneysAssetPath}/ui-space-expansion/barHorizontal_green_left.png`
     );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.LEFT_CAP_SHADOW,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_left.png`
-    );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.MIDDLE_SHADOW,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_mid.png`
-    );
-    this.load.image(
-      HEALTH_BAR_ASSET_KEYS.RIGHT_CAP_SHADOW,
-      `${kenneysAssetPath}/ui-space-expansion/barHorizontal_shadow_right.png`
-    );
 
     this.load.image(
       HEALTH_BAR_ASSET_KEYS.RIGHT_CAP_SHADOW,
