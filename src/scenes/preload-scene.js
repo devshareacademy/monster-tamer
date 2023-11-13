52a9c73f174b6070b71ab674a95b317efc82ee5b
Scott Westover <scottwestover2006@gmail.com>
2023-11-12 23:30:01 -0500

    added background game object to world scene
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -7,6 +7,7 @@ import {
   HEALTH_BAR_ASSET_KEYS,
   MONSTER_ASSET_KEYS,
   UI_ASSET_KEYS,
+  WORLD_ASSET_KEYS,
 } from '../assets/asset-keys.js';
 import { SCENE_KEYS } from './scene-keys.js';
 import { KENNEY_FUTURE_NARROW_FONT_NAME } from '../assets/font-keys.js';
@@ -85,6 +86,9 @@ export class PreloadScene extends Phaser.Scene {
       frameWidth: 48,
       frameHeight: 48,
     });
+
+    // load world assets
+    this.load.image(WORLD_ASSET_KEYS.WORLD_BACKGROUND, `${monsterTamerAssetPath}/map/level_background.png`);
   }
 
   create() {
