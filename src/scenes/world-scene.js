8be2e1f8f8acf94fa07f1c123d31f90a1daee65a
Scott Westover <scottwestover2006@gmail.com>
2023-11-18 20:52:01 -0500

    added npc game objects
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/world-scene.js
+++ src/scenes/world-scene.js
@@ -9,6 +9,7 @@ import { DATA_MANAGER_STORE_KEYS, dataManager } from '../utils/data-manager.js';
 import { getTargetPositionFromGameObjectPositionAndDirection } from '../utils/grid-utils.js';
 import { CANNOT_READ_SIGN_TEXT, SAMPLE_TEXT } from '../utils/text-utils.js';
 import { DialogUi } from '../world/dialog-ui.js';
+import { NPC } from '../world/characters/npc.js';
 
 /**
  * @typedef TiledObjectProperty
@@ -20,6 +21,18 @@ import { DialogUi } from '../world/dialog-ui.js';
 
 const TILED_SIGN_PROPERTY = Object.freeze({
   MESSAGE: 'message',
+});
+
+const CUSTOM_TILED_TYPES = Object.freeze({
+  NPC: 'npc',
+  NPC_PATH: 'npc_path',
+});
+
+const TILED_NPC_PROPERTY = Object.freeze({
+  IS_SPAWN_POINT: 'is_spawn_point',
+  MOVEMENT_PATTERN: 'movement_pattern',
+  MESSAGES: 'messages',
+  FRAME: 'frame',
 });
 
 /*
@@ -45,6 +58,8 @@ export class WorldScene extends Phaser.Scene {
   #signLayer;
   /** @type {DialogUi} */
   #dialogUi;
+  /** @type {NPC[]} */
+  #npcs;
 
   constructor() {
     super({
@@ -105,6 +120,10 @@ export class WorldScene extends Phaser.Scene {
 
     this.add.image(0, 0, WORLD_ASSET_KEYS.WORLD_BACKGROUND, 0).setOrigin(0);
 
+    // create npcs
+    this.#createNPCs(map);
+
+    // create player and have camera focus on the player
     this.#player = new Player({
       scene: this,
       position: dataManager.store.get(DATA_MANAGER_STORE_KEYS.PLAYER_POSITION),
@@ -147,6 +166,10 @@ export class WorldScene extends Phaser.Scene {
     }
 
     this.#player.update(time);
+
+    this.#npcs.forEach((npc) => {
+      npc.update(time);
+    });
   }
 
   #handlePlayerInteraction() {
@@ -224,5 +247,39 @@ export class WorldScene extends Phaser.Scene {
 
   #isPlayerInputLocked() {
     return this.#dialogUi.isVisible;
+  }
+
+  /**
+   * @param {Phaser.Tilemaps.Tilemap} map
+   * @returns {void}
+   */
+  #createNPCs(map) {
+    this.#npcs = [];
+
+    const npcLayers = map.getObjectLayerNames().filter((layerName) => layerName.includes('NPC'));
+    npcLayers.forEach((layerName) => {
+      const layer = map.getObjectLayer(layerName);
+      const npcObject = layer.objects.find((obj) => {
+        return obj.type === CUSTOM_TILED_TYPES.NPC;
+      });
+      if (!npcObject || npcObject.x === undefined || npcObject.y === undefined) {
+        return;
+      }
+
+      /** @type {string} */
+      const npcFrame =
+        /** @type {TiledObjectProperty[]} */ (npcObject.properties).find(
+          (property) => property.name === TILED_NPC_PROPERTY.FRAME
+        )?.value || '0';
+
+      // In Tiled, the x value is how far the object starts from the left, and the y is the bottom of tiled object that is being added
+      const npc = new NPC({
+        scene: this,
+        position: new Phaser.Math.Vector2(npcObject.x, npcObject.y - TILE_SIZE),
+        direction: DIRECTION.DOWN,
+        frame: parseInt(npcFrame, 10),
+      });
+      this.#npcs.push(npc);
+    });
   }
 }
---
+++ src/world/characters/npc.js
@@ -0,0 +1,56 @@
+import Phaser from '../../lib/phaser.js';
+import { Character } from './character.js';
+import { CHARACTER_ASSET_KEYS } from '../../assets/asset-keys.js';
+import { DIRECTION } from '../../common/direction.js';
+import { exhaustiveGuard } from '../../utils/guard.js';
+
+/**
+ * @typedef {Omit<import('./character').CharacterConfig, 'assetKey' | 'assetFrame' | 'idleFrame'> & {frame: number}} NPCConfig
+ */
+
+export class NPC extends Character {
+  /**
+   * @param {NPCConfig} config
+   */
+  constructor(config) {
+    super({
+      ...config,
+      assetKey: CHARACTER_ASSET_KEYS.NPC,
+      origin: new Phaser.Math.Vector2(0, 0),
+      idleFrame: {
+        DOWN: config.frame,
+        UP: config.frame + 1,
+        NONE: config.frame,
+        LEFT: config.frame + 2,
+        RIGHT: config.frame + 2,
+      },
+    });
+
+    this._phaserGameObject.setScale(4);
+  }
+
+  /**
+   * @param {import('../../common/direction.js').Direction} playerDirection
+   * @returns {void}
+   */
+  facePlayer(playerDirection) {
+    switch (playerDirection) {
+      case DIRECTION.DOWN:
+        this._phaserGameObject.setFrame(this._idleFrameConfig.UP).setFlipX(false);
+        break;
+      case DIRECTION.LEFT:
+        this._phaserGameObject.setFrame(this._idleFrameConfig.RIGHT).setFlipX(false);
+        break;
+      case DIRECTION.RIGHT:
+        this._phaserGameObject.setFrame(this._idleFrameConfig.LEFT).setFlipX(true);
+        break;
+      case DIRECTION.UP:
+        this._phaserGameObject.setFrame(this._idleFrameConfig.DOWN).setFlipX(false);
+        break;
+      case DIRECTION.NONE:
+        break;
+      default:
+        exhaustiveGuard(playerDirection);
+    }
+  }
+}
