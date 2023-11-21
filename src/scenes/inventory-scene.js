cfba28ca54ed92d2175cc7353eded5c32feb1a3c
Scott Westover <scottwestover2006@gmail.com>
2023-11-21 12:02:43 -0500

    [GH-42] started implementing inventory scene
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ src/scenes/inventory-scene.js
@@ -0,0 +1,12 @@
+import Phaser from '../lib/phaser.js';
+import { SCENE_KEYS } from './scene-keys.js';
+
+export class InventoryScene extends Phaser.Scene {
+  constructor() {
+    super({ key: SCENE_KEYS.INVENTORY_SCENE });
+  }
+
+  create() {
+    // TODO
+  }
+}
