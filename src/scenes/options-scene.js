4c781781608f5bec37165ab76908805ce2633693
Scott Westover <scottwestover2006@gmail.com>
2023-11-30 08:53:30 -0500

    [GH-53] added comments to options scene for initial layout
    * added comments to options scene to plan out the various components
    and structure we will need
    
    GitHub Issue: https://github.com/devshareacademy/monster-tamer/issues/53
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/options-scene.js
+++ src/scenes/options-scene.js
@@ -8,5 +8,23 @@ export class OptionsScene extends Phaser.Scene {
 
   create() {
     console.log(`[${OptionsScene.name}:create] invoked`);
+
+    // main options container
+
+    // create main option sections
+
+    // create text speed options
+
+    // create battle scene options
+
+    // create battle style options
+
+    // create sound options
+
+    // volume options
+
+    // frame options
+
+    // option details container
   }
 }
