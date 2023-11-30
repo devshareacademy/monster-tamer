c516dd937bb581996c1eef5dbe8125600038a109
Scott Westover <scottwestover2006@gmail.com>
2023-11-30 08:58:02 -0500

    [GH-53] added initial menu container in options scene
    * created nineslice instance for the background of the main menu container
    in the options scene
    
    GitHub Issue: https://github.com/devshareacademy/monster-tamer/issues/53
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/options-scene.js
+++ src/scenes/options-scene.js
@@ -1,15 +1,37 @@
 import Phaser from '../lib/phaser.js';
 import { SCENE_KEYS } from './scene-keys.js';
+import { UI_ASSET_KEYS } from '../assets/asset-keys.js';
+import { NineSlice } from '../utils/nine-slice.js';
 
 export class OptionsScene extends Phaser.Scene {
+  /** @type {Phaser.GameObjects.Container} */
+  #mainContainer;
+  /** @type {NineSlice} */
+  #nineSliceMainContainer;
+
   constructor() {
     super({ key: SCENE_KEYS.OPTIONS_SCENE });
+  }
+
+  init() {
+    console.log(`[${OptionsScene.name}:init] invoked`);
+
+    this.#nineSliceMainContainer = new NineSlice({
+      cornerCutSize: 32,
+      textureManager: this.sys.textures,
+      assetKey: UI_ASSET_KEYS.MENU_BACKGROUND,
+    });
   }
 
   create() {
     console.log(`[${OptionsScene.name}:create] invoked`);
 
+    const { width, height } = this.scale;
+    const optionMenuWidth = width - 200;
+
     // main options container
+    this.#mainContainer = this.#nineSliceMainContainer.createNineSliceContainer(this, optionMenuWidth, 432);
+    this.#mainContainer.setX(100).setY(20);
 
     // create main option sections
 
