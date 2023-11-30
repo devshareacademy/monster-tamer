5f58f5a07fc54495f3a87b5178665f9c2c6a0ca5
Scott Westover <scottwestover2006@gmail.com>
2023-11-30 09:09:49 -0500

    [GH-53] added available options to options scene
    * created text objects representing the available options the player can
    select from
    
    GitHub Issue: https://github.com/devshareacademy/monster-tamer/issues/53
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/options-scene.js
+++ src/scenes/options-scene.js
@@ -2,6 +2,14 @@ import Phaser from '../lib/phaser.js';
 import { SCENE_KEYS } from './scene-keys.js';
 import { UI_ASSET_KEYS } from '../assets/asset-keys.js';
 import { NineSlice } from '../utils/nine-slice.js';
+import { KENNEY_FUTURE_NARROW_FONT_NAME } from '../assets/font-keys.js';
+
+/** @type {Phaser.Types.GameObjects.Text.TextStyle} */
+const OPTIONS_TEXT_STYLE = {
+  fontFamily: KENNEY_FUTURE_NARROW_FONT_NAME,
+  color: '#FFFFFF',
+  fontSize: '30px',
+};
 
 export class OptionsScene extends Phaser.Scene {
   /** @type {Phaser.GameObjects.Container} */
@@ -34,6 +42,19 @@ export class OptionsScene extends Phaser.Scene {
     this.#mainContainer.setX(100).setY(20);
 
     // create main option sections
+    this.add.text(width / 2, 40, 'Options', OPTIONS_TEXT_STYLE).setOrigin(0.5);
+    const menuOptions = ['Text Speed', 'Battle Scene', 'Battle Style', 'Sound', 'Volume', 'Menu Color', 'Close'];
+    const menuOptionPosition = {
+      x: 25,
+      yStart: 55,
+      yIncrement: 55,
+    };
+    menuOptions.forEach((option, index) => {
+      const x = menuOptionPosition.x;
+      const y = menuOptionPosition.yStart + menuOptionPosition.yIncrement * index;
+      const textGameObject = this.add.text(x, y, option, OPTIONS_TEXT_STYLE);
+      this.#mainContainer.add(textGameObject);
+    });
 
     // create text speed options
 
