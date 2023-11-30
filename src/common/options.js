efc0fe1e55c8eff6c98ed32670625e131c051a81
Scott Westover <scottwestover2006@gmail.com>
2023-11-30 16:12:07 -0500

    [GH-53] added logic to update the selected option menu option
    * added input handling to options scene
    * added logic to select an option menu and when the player provides input
    
    GitHub Issue: https://github.com/devshareacademy/monster-tamer/issues/53
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ src/common/options.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {keyof typeof OPTION_MENU_OPTIONS} OptionMenuOptions
+ */
+
+/** @enum {OptionMenuOptions} */
+export const OPTION_MENU_OPTIONS = Object.freeze({
+  TEXT_SPEED: 'TEXT_SPEED',
+  BATTLE_SCENE: 'BATTLE_SCENE',
+  BATTLE_STYLE: 'BATTLE_STYLE',
+  SOUND: 'SOUND',
+  VOLUME: 'VOLUME',
+  MENU_COLOR: 'MENU_COLOR',
+  CONFIRM: 'CONFIRM',
+});
