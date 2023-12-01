7ed9dac5216158e854370e9411b0d77bac6dd8e1
Scott Westover <scottwestover2006@gmail.com>
2023-12-01 13:51:06 -0500

    [GH-37] reverted change tied to preload scene
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -193,7 +193,7 @@ export class PreloadScene extends Phaser.Scene {
     dataManager.loadData();
 
     this.#createAnimations();
-    this.scene.start(SCENE_KEYS.BATTLE_SCENE);
+    this.scene.start(SCENE_KEYS.TITLE_SCENE);
   }
 
   /**
