a46ef2b9551b92304c25dd6f9de29692fb4bd3ea
Scott Westover <scottwestover2006@gmail.com>
2023-12-20 10:15:15 -0500

    [GH-52] added support for data persistance
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -15,6 +15,7 @@ import { SCENE_KEYS } from './scene-keys.js';
 import { KENNEY_FUTURE_NARROW_FONT_NAME } from '../assets/font-keys.js';
 import { WebFontFileLoader } from '../assets/web-font-file-loader.js';
 import { DataUtils } from '../utils/data-utils.js';
+import { dataManager } from '../utils/data-manager.js';
 
 export class PreloadScene extends Phaser.Scene {
   constructor() {
@@ -126,6 +127,8 @@ export class PreloadScene extends Phaser.Scene {
   create() {
     console.log(`[${PreloadScene.name}:create] invoked`);
     this.#createAnimations();
+    // attempt to populate data manager with saved data
+    dataManager.loadData();
     this.scene.start(SCENE_KEYS.OPTIONS_SCENE);
   }
 
