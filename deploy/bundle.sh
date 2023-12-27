2d6e8eff4362d3db93b67d7b8ce9b5ae85a41688
Scott Westover <scottwestover2006@gmail.com>
2023-12-27 10:53:54 -0500

    [GH-76] updated bundle script to copy source code
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- deploy/bundle.sh
+++ deploy/bundle.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+# download required assets for the game
 curl https://github.com/devshareacademy/monster-tamer/releases/download/assets/all-game-assets.zip -LO
 rm -rf assets
 mkdir assets
@@ -8,3 +9,9 @@ unzip all-game-assets.zip -d assets
 rm -rf assets/__MACOSX
 
 # bundle up the game now
+rm -rf dist
+mkdir dist
+mkdir dist/src
+cp -R ../src/. dist/src
+cp ../index.html dist/index.html
+mv assets/assets dist/assets
