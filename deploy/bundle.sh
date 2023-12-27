0ca492d676b20d22d670d8a17e78bf8a2aaf4fb8
Scott Westover <scottwestover2006@gmail.com>
2023-12-27 11:03:44 -0500

    [GH-76] added github ci workflow for bundling
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- deploy/bundle.sh
+++ deploy/bundle.sh
@@ -3,15 +3,13 @@ set -e
 
 # download required assets for the game
 curl https://github.com/devshareacademy/monster-tamer/releases/download/assets/all-game-assets.zip -LO
-rm -rf assets
-mkdir assets
-unzip all-game-assets.zip -d assets
-rm -rf assets/__MACOSX
-
-# bundle up the game now
 rm -rf dist
 mkdir dist
+unzip all-game-assets.zip -d dist
+rm -rf dist/__MACOSX
+rm -rf dist/assets/.gitkeep
+
+# bundle up the game now
 mkdir dist/src
-cp -R ../src/. dist/src
-cp ../index.html dist/index.html
-mv assets/assets dist/assets
+cp -R ./src/. dist/src
+cp ./index.html dist/index.html
