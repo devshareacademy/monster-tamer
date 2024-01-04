a0d951b28a919a30c13c3fda01cb7339b67bd33b
Scott Westover <scottwestover2006@gmail.com>
2024-01-03 20:41:14 -0500

    [GH-101] created initial plan for monster party scene
    created the initial plan
    for properties and comments for elements that need to be created in the scene.
    
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/monster-party-scene.js
+++ src/scenes/monster-party-scene.js
@@ -1,8 +1,43 @@
+import { HealthBar } from '../battle/ui/health-bar.js';
 import { BaseScene } from './base-scene.js';
 import { SCENE_KEYS } from './scene-keys.js';
 
 export class MonsterPartyScene extends BaseScene {
+  /** @type {Phaser.GameObjects.Image[]} */
+  #monsterPartyBackgrounds;
+  /** @type {Phaser.GameObjects.Image} */
+  #cancelButton;
+  /** @type {Phaser.GameObjects.Text} */
+  #infoTextGameObject;
+  /** @type {HealthBar[]} */
+  #healthBars;
+  /** @type {Phaser.GameObjects.Text[]} */
+  #healthBarTextGameObjects;
+
   constructor() {
     super({ key: SCENE_KEYS.MONSTER_PARTY_SCENE });
+  }
+
+  /**
+   * @returns {void}
+   */
+  init() {
+    super.init();
+
+    this.#monsterPartyBackgrounds = [];
+    this.#healthBars = [];
+    this.#healthBarTextGameObjects = [];
+  }
+
+  /**
+   * @returns {void}
+   */
+  create() {
+    super.create();
+
+    // create custom background
+    // create back button
+    // create info container
+    // create monsters in party
   }
 }
