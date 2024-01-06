d4ae26df71b88c8a284e7d5c81e5b11cb052a755
Scott Westover <scottwestover2006@gmail.com>
2024-01-06 11:39:16 -0500

    [GH-102] added tweakpane gui
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/test-scene.js
+++ src/scenes/test-scene.js
@@ -51,7 +51,96 @@ export class TestScene extends Phaser.Scene {
   /**
    * @returns {void}
    */
-  #addDataGui() {}
+  #addDataGui() {
+    const pane = new TweakPane.Pane();
+
+    const f1 = pane.addFolder({
+      title: 'Monsters',
+      expanded: true,
+    });
+    const playerMonsterFolder = f1.addFolder({
+      title: 'Player',
+      expanded: true,
+    });
+    playerMonsterFolder.addBinding(this.#playerMonster, 'x', {
+      min: 0,
+      max: 1024,
+      step: 1,
+    });
+    playerMonsterFolder.addBinding(this.#playerMonster, 'y', {
+      min: 0,
+      max: 576,
+      step: 1,
+    });
+
+    const enemyMonsterFolder = f1.addFolder({
+      title: 'Enemy',
+      expanded: true,
+    });
+    enemyMonsterFolder.addBinding(this.#enemyMonster, 'x', { readonly: true });
+    enemyMonsterFolder.addBinding(this.#enemyMonster, 'y', { readonly: true });
+
+    const f2Params = {
+      attack: this.#selectedAttack,
+      x: 745,
+      y: 120,
+    };
+    const f2 = pane.addFolder({
+      title: 'Attacks',
+      expanded: true,
+    });
+    f2.addBinding(f2Params, 'attack', {
+      options: {
+        [ATTACK_KEYS.SLASH]: ATTACK_KEYS.SLASH,
+        [ATTACK_KEYS.ICE_SHARD]: ATTACK_KEYS.ICE_SHARD,
+      },
+    }).on('change', (ev) => {
+      if (ev.value === ATTACK_KEYS.ICE_SHARD) {
+        this.#selectedAttack = ATTACK_KEYS.ICE_SHARD;
+        f2Params.x = this.#iceShardAttack.gameObject.x;
+        f2Params.y = this.#iceShardAttack.gameObject.y;
+        f2.refresh();
+        return;
+      }
+      if (ev.value === ATTACK_KEYS.SLASH) {
+        this.#selectedAttack = ATTACK_KEYS.SLASH;
+        f2Params.x = this.#slashAttack.gameObject.x;
+        f2Params.y = this.#slashAttack.gameObject.y;
+        f2.refresh();
+        return;
+      }
+    });
+
+    const playAttackButton = f2.addButton({
+      title: 'Play',
+    });
+    playAttackButton.on('click', () => {
+      if (this.#selectedAttack === ATTACK_KEYS.ICE_SHARD) {
+        this.#iceShardAttack.playAnimation();
+        return;
+      }
+
+      if (this.#selectedAttack === ATTACK_KEYS.SLASH) {
+        this.#slashAttack.playAnimation();
+        return;
+      }
+    });
+
+    f2.addBinding(f2Params, 'x', {
+      min: 0,
+      max: 1024,
+      step: 1,
+    }).on('change', (ev) => {
+      this.#updateAttackGameObjectPosition('x', ev.value);
+    });
+    f2.addBinding(f2Params, 'y', {
+      min: 0,
+      max: 576,
+      step: 1,
+    }).on('change', (ev) => {
+      this.#updateAttackGameObjectPosition('y', ev.value);
+    });
+  }
 
   /**
    * @param {'x' | 'y'} param
