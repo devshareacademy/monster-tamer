76e11051bde32a1cbb1dfd66335b815f4ba3b5c0
Scott Westover <scottwestover2006@gmail.com>
2024-03-05 10:50:21 -0500

    [GH-118] added bg audio support
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/preload-scene.js
+++ src/scenes/preload-scene.js
@@ -12,6 +12,7 @@ import {
   TITLE_ASSET_KEYS,
   UI_ASSET_KEYS,
   WORLD_ASSET_KEYS,
+  AUDIO_ASSET_KEYS,
 } from '../assets/asset-keys.js';
 import { SCENE_KEYS } from './scene-keys.js';
 import { KENNEY_FUTURE_NARROW_FONT_NAME } from '../assets/font-keys.js';
@@ -164,6 +165,17 @@ export class PreloadScene extends BaseScene {
       `${monsterTamerAssetPath}/ui/inventory/bag_background.png`
     );
     this.load.image(INVENTORY_ASSET_KEYS.INVENTORY_BAG, `${monsterTamerAssetPath}/ui/inventory/bag.png`);
+
+    // load audio
+    this.load.setPath('assets/audio/xDeviruchi');
+    this.load.audio(AUDIO_ASSET_KEYS.MAIN, 'And-the-Journey-Begins.wav');
+    this.load.audio(AUDIO_ASSET_KEYS.BATTLE, 'Decisive-Battle.wav');
+    this.load.audio(AUDIO_ASSET_KEYS.TITLE, 'Title-Theme.wav');
+    this.load.setPath('assets/audio/leohpaz');
+    this.load.audio(AUDIO_ASSET_KEYS.CLAW, '03_Claw_03.wav');
+    this.load.audio(AUDIO_ASSET_KEYS.GRASS, '03_Step_grass_03.wav');
+    this.load.audio(AUDIO_ASSET_KEYS.ICE, '13_Ice_explosion_01.wav');
+    this.load.audio(AUDIO_ASSET_KEYS.FLEE, '51_Flee_02.wav');
   }
 
   /**
