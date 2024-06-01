84eefeae650ac97883096782a94ef2d96aa68971
Scott Westover <scottwestover2006@gmail.com>
2024-06-01 01:07:21 -0400

    [GH-130] refactored world menu class
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

---
+++ src/world/world-menu.js
@@ -0,0 +1,21 @@
+import { Menu } from '../common/menu/menu.js';
+
+/**
+ * @typedef {keyof typeof MENU_OPTIONS} MenuOptions
+ */
+
+/** @enum {MenuOptions} */
+export const MENU_OPTIONS = Object.freeze({
+  MONSTERDEX: 'MONSTERDEX',
+  MONSTERS: 'MONSTERS',
+  BAG: 'BAG',
+  SAVE: 'SAVE',
+  OPTIONS: 'OPTIONS',
+  EXIT: 'EXIT',
+});
+
+export class WorldMenu extends Menu {
+  constructor(scene) {
+    super(scene, [MENU_OPTIONS.MONSTERS, MENU_OPTIONS.BAG, MENU_OPTIONS.SAVE, MENU_OPTIONS.EXIT]);
+  }
+}
