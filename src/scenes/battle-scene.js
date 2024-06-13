2b114591ed8d6f5dd2a877cb7e8dc579c6a2adf6
Scott Westover <scottwestover2006@gmail.com>
2024-06-13 09:11:44 -0400

    [GH-122] added monster catching logic
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -20,6 +20,7 @@ import { exhaustiveGuard } from '../utils/guard.js';
 import { Ball } from '../battle/ball.js';
 import { sleep } from '../utils/time-utils.js';
 import { generateUuid } from '../utils/random.js';
+import { calculateMonsterCaptureResults } from '../utils/catch-utils.js';
 
 const MONSTER_PARTY_UI_ALPHA = Object.freeze({
   active: 1,
@@ -756,12 +757,30 @@ export class BattleScene extends BaseScene {
     this.#battleStateMachine.addState({
       name: BATTLE_STATES.CAPTURE_ITEM_USED,
       onEnter: async () => {
-        const wasCaptured = true;
+        // we throw the monster ball to attempt to capture the monster
+        // play ball shake animation depending on success of capture
+        // 3 shakes - caught, 2 shakes - off by 10, 1 shakes off by 30, 0 shakes off by more than 30
+        // 1. throw ball animation, 2. shake ball animation, 3. break animation if needed, 4. monster re-appear if needed
+
+        const captureResults = calculateMonsterCaptureResults(this.#activeEnemyMonster);
+        const diffInCapture = captureResults.requiredCaptureValue - captureResults.actualCaptureValue;
+        let numberOfShakes = 0;
+        if (diffInCapture <= 10) {
+          numberOfShakes = 2;
+        } else if (diffInCapture <= 30) {
+          numberOfShakes = 1;
+        }
+        if (captureResults.wasCaptured) {
+          numberOfShakes = 3;
+        }
+
         await this.#ball.playThrowBallAnimation();
         await this.#activeEnemyMonster.playCatchAnimation();
-        await this.#ball.playShakeBallAnimation(3);
+        if (numberOfShakes > 0) {
+          await this.#ball.playShakeBallAnimation(numberOfShakes - 1);
+        }
 
-        if (wasCaptured) {
+        if (captureResults.wasCaptured) {
           this.#monsterCaptured = true;
           this.#battleStateMachine.setState(BATTLE_STATES.POST_ATTACK_CHECK);
           return;
